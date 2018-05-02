67198e40c3fe00be2490fc3e7bb21e6a715146ff
Daniel Lanier <lanier4@mntl-dhcp108.mntl.illinois.edu>
2018-05-02 16:05:47 -0500

    notebook documentation example

--- src/utility.py
+++ src/utility.py
@@ -2,7 +2,7 @@
 lanier4@illinois.edu
 """
 import os
-import json
+import time
 from inspect import getmembers, isfunction, getsource, signature
 
 conscientious_message = 'USER MISTAKE -- NOT AN ERROR'
@@ -10,6 +10,7 @@ conscientious_message = 'USER MISTAKE -- NOT AN ERROR'
 def show_conscientious_message():
     """ avoid throwing an error: just show_conscientious_message() """
     print(conscientious_message)
+
 
 def display_module_functions(imported_module, show_imported_functions=False):
     """ Usage: display_module_functions(any_module) 
@@ -45,27 +46,44 @@ def display_module_functions(imported_module, show_imported_functions=False):
                 print(docs_string,'\n')
 
 
+def save_installed_versions_to_txt(dir_name=None, file_name=None, time_stamp=False):
+    """ write the pip3 view of installed packages """
+    if dir_name is None:
+        dir_name = os.getcwd()
+    if file_name is None:
+        file_name = 'py_module_versions_' + time.strftime("%a_%d_%b_%Y_%H_%M_%S", time.localtime()) + '.txt'
+    if time_stamp == True:
+        file_name = file_name + time.strftime("%a_%d_%b_%Y_%H_%M_%S", time.localtime()) + '.txt'
+    
+    full_file_name = os.path.join(dir_name, file_name)
+    pip_str = 'pip3 list &> ' + full_file_name
+    os.system(pip_str)
+
+    return full_file_name
+
+
 def get_pip3_versions_dict():
     """ Usage: get_pip3_versions_dict()
          
     Returns:
         pip3_vd:                pip3 installed versions dictionary
     """
-    file_name = 'pip_tst_list.txt'
+    file_name = 'pip_tmp_list.txt'
     pip_str = 'pip3 list &> ' + file_name
     os.system(pip_str)
-
+    
     pip3_vd = {}
     with open(file_name, 'r') as fh:
         for line in fh:
             n, N = line.split()
-            if n[0] == '-' or n == 'Package':
+            if n[-5:] == '-----' or n[-5:] == '----:' or n == 'Package':
                 continue
             pip3_vd[n] = N
 
     os.remove(file_name)
 
     return pip3_vd
+
 
 def read_version_dict_file(versions_file_full_path):
     """ get the installed versions from a version list 
@@ -99,43 +117,8 @@ def read_version_dict_file(versions_file_full_path):
             
     return package_names_dict
 
-def get_version_dict(package_names_list, display_version_numbers=True):
-    """ get the installed versions from a version list 
-    
-    Args:
-        package_names_list:         a python list of pypi packages
-        display_version_numbers:    print the output to the command line
-        
-    Returns:
-        actual_version_dict:        python dict of form { package_name: installed_version_number }
-    """
-    actual_version_dict = {}
-    for lib_in in package_names_list:
-        IMPORTED_SUCCESSFULLY = False
-        installed_version = '-.--.-'
-        try:
-            one_module = importlib.import_module(lib_in)
-            IMPORTED_SUCCESSFULLY = True
-            installed_version = one_module.__version__
 
-        except:
-            pass
-
-        if IMPORTED_SUCCESSFULLY == True:
-            actual_version_dict[lib_in] = installed_version
-        else:
-            actual_version_dict[lib_in] = 'Not Found'
-
-        if display_version_numbers == True:
-            if IMPORTED_SUCCESSFULLY == True:
-                print('%20s:  %s'%(lib_in, installed_version))
-            else:
-                print('%20s:  %s'%(lib_in, 'Not Found'))
-
-    return actual_version_dict
-
-
-def display_dictionary(python_dict):
+def display_version_dictionary(python_dict):
     """ std out formatted display of a python dictionary 
     Args:
         python_dict:    a python dictionary {'name1':'def1', 'name2':'def2',... }
@@ -147,67 +130,10 @@ def display_dictionary(python_dict):
     L = max([len(o) for o in list(python_dict.keys())])
     format_string = '%s%i%s: %s'%('%', L, 's', '%s')
     for key_n, value_n in python_dict.items():
+        if key_n[-3:] == '---' or key_n[-3:] == '--:' or key_n == 'Packages':
+            continue
         print(format_string%(key_n, value_n))
         
-
-def dict_file_read(file_name):
-    """ read a text file into a python dict - intended for version installation data """
-    output_dict = None
-    try:
-        with open(file_name, 'r') as file:
-             output_dict = file.read(json.loads(input_dict))
-    except:
-        pass
-    
-    if output_dict is None:
-        try:
-            output_dict = read_version_dict_file(file_name)
-        except:
-            print(file_name,'failed to load')
-            pass
-    
-    return output_dict
-
-def dict_write_file(input_dict, file_name='dict_file.txt'):
-    """ write a python dict to a text file - intended for version installation data """
-    try:
-        with open(file_name, 'w') as file:
-             file.write(json.dumps(input_dict))
-    except:
-        print(file_name,'fails to write')
-        pass
-
-def read_version_dict_file(versions_file_full_path):
-    """ get the installed versions from a version list 
-    
-    Args:
-        versions_file_full_path:    valid path to a text file created with unix command: pip3 list &> ver.txt
-        display_version_numbers:    print the output to the command line
-        
-    Returns:
-        actual_version_dict:        python dict of form { package_name: installed_version_number }
-    """
-    try:
-        with open(versions_file_full_path, 'r') as fd:
-            package_names_lines = fd.read().splitlines()
-    except:
-        print(versions_file_full_path, '\nfailed to open\n')
-        return None
-    
-    ignore_name_list = ['Package']
-    ignore_version_list = ['-------']
-    package_names_dict = {}    
-    for line in package_names_lines:
-        str_list = line.split()
-        package_name = str_list[0]
-        package_version = str_list[1]
-        if package_name in ignore_name_list or package_version in ignore_version_list:
-            continue
-            print('skipp', package_name)
-        else:
-            package_names_dict[package_name] = package_version
-            
-    return package_names_dict
 
 def get_versions_intallation_dict(required_versions_dict, installed_versions_dict=None):
     """ required versions compare installed versions """
@@ -225,6 +151,7 @@ def get_versions_intallation_dict(required_versions_dict, installed_versions_dic
             
     return comparison_dict
 
+
 def get_installed_differences_dict(required_versions_dict):
     """ returns dict of versions required vs versions installed that are different """
     comparison_dict = get_versions_intallation_dict(required_versions_dict)
@@ -234,3 +161,69 @@ def get_installed_differences_dict(required_versions_dict):
             differences_dict[package_name] = compare_list
             
     return differences_dict
+
+
+def read_version_dict_file(versions_file_full_path):
+    """ get the installed versions from a version list 
+    
+    Args:
+        versions_file_full_path:    valid path to a text file created with unix command: pip3 list &> ver.txt
+        display_version_numbers:    print the output to the command line
+        
+    Returns:
+        actual_version_dict:        python dict of form { package_name: installed_version_number }
+    """
+    try:
+        with open(versions_file_full_path, 'r') as fd:
+            package_names_lines = fd.read().splitlines()
+    except:
+        print(versions_file_full_path, '\nfailed to open\n')
+        return None
+    
+    ignore_name_list = ['Package']
+    ignore_version_list = ['-------']
+    package_names_dict = {}    
+    for line in package_names_lines:
+        str_list = line.split()
+        package_name = str_list[0]
+        package_version = str_list[1]
+        if package_name in ignore_name_list or package_version in ignore_version_list:
+            continue
+            print('skipp', package_name)
+        else:
+            package_names_dict[package_name] = package_version
+            
+    return package_names_dict
+
+
+def dict_file_read(file_name):
+    """ read a text file into a python dict - intended for version installation data """
+    output_dict = None
+    try:
+        with open(file_name) as fd:
+            for line in fd:
+                (key, val) = line.split()
+                if key[-2:] == '-:':
+                    continue
+                output_dict[key] = val
+    except:
+        pass
+    
+    if output_dict is None:
+        try:
+            output_dict = read_version_dict_file(file_name)
+        except:
+            print(file_name,'failed to load')
+            pass
+    
+    return output_dict
+
+
+def dict_write_file(input_dict, file_name='dict_file.txt'):
+    """ write a python dict to a text file - intended for version installation data """
+    try:
+        with open(file_name, 'w') as file:
+             file.write(json.dumps(input_dict))
+    except:
+        print(file_name,'fails to write')
+        pass
