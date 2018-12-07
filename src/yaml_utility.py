1397ab943cb79bdef29c100d80f29d53fb1497c0
dlanier <lanier4@illinois.edu>
2018-12-07 09:03:40 -0600

    inc json df

--- src/yaml_utility.py
+++ src/yaml_utility.py
@@ -6,11 +6,33 @@ mradmstr514226508@gmail.com
 """
 import os
 import pandas as pd
+import yaml
 
-import knpackage.toolbox as kn
 
-def get_parameter_keys_dict(yaml_dir, parameter_key_dict=None, methods_list=None):
-    """  get the list of yaml files in the yaml_dir, the list of methods in all those, and a dictionary of key types
+def get_run_parameters(run_directory, run_file):
+    """ Read system input arguments run directory name and run_file into a dictionary.
+
+    Args:
+        run_directory: directory where run_file is expected.
+
+    Returns:
+        run_parameters: python dictionary of name - value parameters.
+    """
+    run_file_name = os.path.join(run_directory, run_file)
+    with open(run_file_name, 'r') as file_handle:
+        run_parameters = yaml.load(file_handle)
+    if not isinstance(run_parameters, dict):
+        run_parameters = {}
+
+    # run_parameters["run_directory"] = run_directory
+    run_parameters["run_file"] = run_file
+
+    return run_parameters
+
+
+def get_parameter_keys_dict(yaml_dir, parameter_key_dict=None, methods_list=None, file_ext=['.yml', '.json']):
+    """ get the list of yaml files in the yaml_dir, the list of methods in all those,
+        and a dictionary of key types
 
     Args:
         yaml_dir:           path the the yaml files
@@ -33,11 +55,11 @@ def get_parameter_keys_dict(yaml_dir, parameter_key_dict=None, methods_list=None
     for f in os.listdir(yaml_dir):
         ff = os.path.join(yaml_dir, f)
         some_nombre, nombre_ext = os.path.splitext(f)
-        if os.path.isfile(ff) and nombre_ext == '.yml':
+        if os.path.isfile(ff) and nombre_ext in file_ext:
             yaml_files_list.append(f)
 
     for f_name in yaml_files_list:
-        run_parameters = kn.get_run_parameters(yaml_dir, f_name)
+        run_parameters = get_run_parameters(yaml_dir, f_name)
         for k, v in run_parameters.items():
             if k not in parameter_key_dict.keys():
                 parameter_key_dict[k] = type_dict[type(v)]
@@ -45,29 +67,36 @@ def get_parameter_keys_dict(yaml_dir, parameter_key_dict=None, methods_list=None
                 methods_list.append(v)
 
     methods_list = list(set(methods_list))
-    return parameter_key_dict, methods_list, sorted(yaml_files_list)
+    return parameter_key_dict, methods_list, yaml_files_list
 
 
-def get_yaml_df(yaml_dir, fill_string='not used'):
-    """ Usage:  yaml_file_df = get_yaml_df(directory_with_yaml_files, fill_string)
-    datafrme: rows are keys, columns are yaml file names: fill by yaml file values or unused fill string
+def show_parameter_keys_dict(parameter_key_dict):
+    """ formatted display of dictionary key value pairs
+    """
+    for k, v in parameter_key_dict.items():
+        print('%30s: %s' % (k, v))
+
+
+def get_yaml_df(yaml_dir, UNUSED_FILL='not used'):
+    """ read the yaml files in 'yaml_dir' and constuct a datafrme with rows of keys, columns of file names and
+        filled with the each yaml file values.
     Args:
         yaml_dir:       path the the yaml files
-        fill_string:    string to use for fill where a yaml file has no entry for a key (row name)
+        UNUSED_FILL:    string to use for fill where a yaml file has no entry for a key (row name)
     """
     key_dict, methods_list, yaml_files_list = get_parameter_keys_dict(yaml_dir)
     if 'run_file' in key_dict.keys():
         key_dict.pop('run_file')
-
-    yaml_file_df = pd.DataFrame(data=None, index=sorted(list(key_dict.keys())), columns=None)
+    yaml_file_df = pd.DataFrame(data=None, index=key_dict.keys(), columns=None)
 
     for f_name in yaml_files_list:
-        run_parameters = kn.get_run_parameters(yaml_dir, f_name)
+        run_parameters = get_run_parameters(yaml_dir, f_name)
         if 'run_file' in run_parameters.keys():
             run_parameters.pop('run_file')
 
-        yaml_file_df[f_name] = fill_string
+        yaml_file_df[f_name] = UNUSED_FILL
         for k, v in run_parameters.items():
             yaml_file_df[f_name].loc[k] = v
 
-    return yaml_file_df.T
+    return yaml_file_df
+
